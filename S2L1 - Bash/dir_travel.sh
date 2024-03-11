732746434e67efebe81d65963f9629b254bc1294
den4ik2975 <98264297+den4ik2975@users.noreply.github.com>
2024-03-12 00:53:29 +0300

    -

--- S2L1 - Bash/dir_travel.sh
+++ S2L1 - Bash/dir_travel.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-# Проверяем была директория передана как аргумент
-if [ "$#" -ne 1 ]; then
+show_usage() {
     echo "Usage: $0 <directory>"
+    echo "Traverse the specified directory and display full paths of subdirectories."
+}
+
+# Проверка на аргументы
+if [ $# -ne 1 ]; then
+    echo "Error: Incorrect number of arguments."
+    show_usage
     exit 1
 fi
 
-DIRECTORY=$1
+# Проврека на ?
+if [ "$1" == "?" ]; then
+    show_usage
+    exit 0
+fi
 
-# Проверяем есть ли директория
-if [ ! -d "$DIRECTORY" ]; then
-    echo "Error: Directory $DIRECTORY does not exist."
+# Проверка на наличие введенной директории
+if [ ! -d "$1" ]; then
+    echo "Error: Directory not found."
     exit 1
 fi
 
-# Рекурсивно обходим директории
-find "$DIRECTORY" -type d -print
+# Обходим директории
+find "$1" -type d -print
+
+exit 0
